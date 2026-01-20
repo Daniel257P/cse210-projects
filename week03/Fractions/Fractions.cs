b28bcb8598517f87ede2887581ce6893a8094b53
daniel257p <dpaulinopolanco@byupathway.edu>
2026-01-20 10:33:19 -0600

    Add Fractions class with constructors and methods for fraction representation and decimal conversion

---
+++ week03/Fractions/Fractions.cs
@@ -0,0 +1,65 @@
+using System.Runtime.Versioning;
+
+public class Fractions
+{
+    private int _top;
+    private int _bottom; 
+public Fractions()
+    {
+        _top = 1;
+        _bottom = 1;
+    }
+
+public Fractions(int numerador)
+    {
+        _top = numerador;
+        _bottom = 1;
+    }
+public Fractions(int top, int bottom)
+    {
+        _top = top;
+        _bottom = bottom; 
+    }
+     public string GetFractionString()
+    {
+        string text = $"{_top}/{_bottom}";
+        return text;
+    }
+
+    public double GetDecimalValue()
+    {
+        return (double)_top / (double)_bottom;
+    }
+
+}
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
