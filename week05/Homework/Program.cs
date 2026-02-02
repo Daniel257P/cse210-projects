cc55a009a9642d5ef75ef1eb570c3148011fb38a
daniel257p <dpaulinopolanco@byupathway.edu>
2026-02-02 11:33:36 -0600

    practicing inheritance. added three classes tehir attributes and methos. Week 5 learning activities

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        MathAssignment student1 = new MathAssignment("Juan Ramon", "Multiplication","7.3", "8-19");
+        Console.WriteLine(student1.GetHomeworkList());
+
+        WritingAssignment student2 = new WritingAssignment("Mary Waters", "European History","The Causes of World War II by Mary Waters");
+        Console.WriteLine(student2.GetWritingInformation());
     }
 }
