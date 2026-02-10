5b31b2db435bdb0e952dfef6838bd66e5bcd1e18
daniel257p <dpaulinopolanco@byupathway.edu>
2026-02-10 10:25:06 -0600

    shapes project using polymorphism

---
+++ week06/Shapes/Circle.cs
@@ -0,0 +1,17 @@
+namespace Shapes
+{
+    public class Circle : Shape
+    {
+        private double _radius;
+
+        public Circle(string color, double radius): base (color)
+        {
+            _radius = radius;
+        }
+
+        public override double GetArea()
+        {
+            return 3.14159 * _radius * _radius;
+        }
+    }
+}
--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -1,9 +1,24 @@
 using System;
+using Shapes;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        List<Shapes.Shape> shapes = new List<Shapes.Shape>();
+        
+        Square square = new Square("Blue", 4);
+        shapes.Add(square);
+
+        Rectangle rectangle = new Rectangle("Red", 5, 3);
+        shapes.Add(rectangle);  
+
+        Circle circle = new Circle("Green", 2);
+        shapes.Add(circle); 
+
+        foreach (Shapes.Shape shape in shapes)
+        {
+            Console.WriteLine($"Shape: {shape.GetType().Name}, Color: {shape.GetColor()}, Area: {shape.GetArea()}");
+        }
     }
 }
