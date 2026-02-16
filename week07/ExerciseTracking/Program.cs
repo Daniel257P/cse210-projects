10fdbeb6c62e5e5a921459fe15ed19c7e2f6d9ed
daniel257p <dpaulinopolanco@byupathway.edu>
2026-02-16 11:46:58 -0600

    Exercise Tracking program

--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the ExerciseTracking Project.");
+        List<Activity> activities = new List<Activity>();
+
+        activities.Add(new Running("04 Nov 2025", 30, 3.0));
+        activities.Add(new Cycling("04 Nov 2025", 30, 6.0));
+        activities.Add(new Swimming("04 Nov 2025", 30, 20));
+
+        foreach (Activity a in activities)
+        {
+            Console.WriteLine(a.GetSummary());
+        }
     }
 }
